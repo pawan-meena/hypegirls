07d3d67689adbfd62829df67544fd0bba351147e
pawan-meena <pawanmeenazee@outlook.com>
2022-05-01 15:25:32 +0530

    check

--- js/scripts.js
+++ js/scripts.js
@@ -107,3 +107,174 @@ $(document).ready(function () {
 	});
 
 })(jQuery);
+var Slider = (function() {
+    var initSlider = function() {
+        var dir = $("html").attr("dir");
+        var swipeHandler = new Hammer(document.getElementById("slider"));
+        swipeHandler.on('swipeleft', function(e) {
+            if (dir == "rtl")
+                $(".arrow-left").trigger("click");
+            else
+                $(".arrow-right").trigger("click");
+        });
+
+        swipeHandler.on('swiperight', function(e) {
+            if (dir == "rtl")
+                $(".arrow-right").trigger("click");
+            else
+                $(".arrow-left").trigger("click");
+        });
+
+        $(".arrow-right , .arrow-left").click(function(event) {
+            var nextActiveSlide = $(".slide.active").next();
+
+            if ($(this).hasClass("arrow-left"))
+                nextActiveSlide = $(".slide.active").prev();
+
+            if (nextActiveSlide.length > 0) {
+                var nextActiveIndex = nextActiveSlide.index();
+                $(".dots span").removeClass("active");
+                $($(".dots").children()[nextActiveIndex]).addClass("active");
+
+                updateSlides(nextActiveSlide);
+            }
+        });
+
+        $(".dots span").click(function(event) {
+            var slideIndex = $(this).index();
+            var nextActiveSlide = $($(".slider").children()[slideIndex]);
+            $(".dots span").removeClass("active");
+            $(this).addClass("active");
+
+            updateSlides(nextActiveSlide);
+        });
+
+        var updateSlides = function(nextActiveSlide) {
+            var nextActiveSlideIndex = $(nextActiveSlide).index();
+
+            $(".slide").removeClass("prev-1");
+            $(".slide").removeClass("next-1");
+            $(".slide").removeClass("active");
+            $(".slide").removeClass("prev-2");
+            $(".slide").removeClass("next-2");
+
+            nextActiveSlide.addClass("active");
+
+            nextActiveSlide.prev().addClass("prev-1");
+            nextActiveSlide.prev().prev().addClass("prev-2");
+            nextActiveSlide.addClass("active");
+            nextActiveSlide.next().addClass("next-1");
+            nextActiveSlide.next().next().addClass("next-2");
+        }
+
+        var updateToNextSlide = function(nextActiveSlide)
+        {
+            
+        }
+    }
+    return {
+        init: function() {
+            initSlider();
+        }
+    }
+})();
+
+$(function() {
+    Slider.init();
+});
+
+
+
+const changeValues = () => {
+const inc = document.querySelector(".counter--arrow-inc > span");
+const dec = document.querySelector(".counter--arrow-dec > span");
+const timer = document.querySelector(".counter");
+const output = document.querySelector(".counter--output");
+const outpute = document.querySelector(".counter--outpute");
+const singleStep = "1";
+const singleStepe = singleStep*0.08+" ETH ";
+const medStep = "5";
+const lgStep = "25";
+const largeInt = "500";
+const medInt = "250";
+const smInt = "100";
+let step = "1";
+let stepInterval = 1;
+let intTime = largeInt;
+
+output.setAttribute("value", singleStep); // Initialize input value
+outpute.setAttribute("value", singleStepe);
+timer.addEventListener("mousedown", e => {
+// Start timer to record length of mousedown event
+const startTime = new Date().getTime();
+output.setAttribute("type", "number");
+
+stepInterval = () => {
+const newTime = new Date().getTime();
+const elapsedTime = newTime - startTime;
+
+if (elapsedTime === 0 && elapsedTime < 3000) {
+step = singleStep;
+intTime = largeInt;
+}
+// Update step and interval values
+if (elapsedTime > 3000 && elapsedTime < 6000) {
+step = medStep;
+intTime = medInt;
+}
+
+if (elapsedTime > 6000) {
+step = lgStep;
+intTime = smInt;
+}
+
+// remove click event from decrement button when value is zero
+if (output.value === "0") {
+dec.parentElement.classList.add('js-noevent');
+} else {
+dec.parentElement.classList.remove('js-noevent');
+}
+
+if (inc.parentElement.contains(e.target)) {
+  // on click increment
+  output.stepUp(step);
+  output.setAttribute('value', output.value);
+  output.setAttribute('step', step);
+  const vv = $(".counter--output").val();
+$(".counter--outpute").val(vv*0.08 + " ETH ");
+  exit;
+}
+
+if (dec.parentElement.contains(e.target)) {
+  // on click decrement
+  output.stepDown(step);
+  output.setAttribute('value', output.value);
+  output.setAttribute('step', step);
+  const vvvs = $(".counter--output").val();
+  if (vvvs<=0) {
+const vvvs = $(".counter--output").val(0);
+$(".counter--outpute").val(0 + " ETH ");
+}else{
+  const vvv = $(".counter--outpute").val();
+  let num = parseFloat(vvv).toFixed(2) - parseFloat(0.08).toFixed(2);
+ let text=num.toString();
+  let result = text.slice(0, 4);
+  $(".counter--outpute").val(result+ " ETH ");}
+  exit;
+}
+
+// Call recursively
+count = setTimeout(stepInterval, intTime);
+};
+stepInterval();
+});
+};
+
+window.addEventListener("mouseup", () => {
+clearTimeout(count);
+});
+
+changeValues();
+
+
+
